c9779ff22debfed9edb42afd0da0174dcf5a1985
Samuel <samueldavi6306@gmail.com>
2024-07-10 01:57:18 -0300

    commit2

---
+++ Java Script/break-continue.ts
@@ -0,0 +1,23 @@
+const FRUTAS1: string[] = ["Banana", "Abacaxi", "Limao", "Uva"]
+
+for(let item of FRUTAS1){
+    if(item == "Limao"){
+        break // break e a mesma coisa que parar, ou seja se a condica acima e true pare !
+
+    }
+    console.log(item)
+
+}
+
+
+const FRUTAS2: string[] = ["Banana", "Abacaxi", "Limao", "Uva"]
+
+for(let item3 of FRUTAS2){
+    if(item3 == "Limao"){
+        continue // continue e a mesma coisa que imprimir todos os elementos menos o limao 
+
+    }
+    console.log(item3)
+
+}
+
---
+++ Java Script/for-of.ts
@@ -0,0 +1,8 @@
+const FRUTAS: string[] = ["Banana", "Abacaxi", "Limao", "Uva"]
+
+for(let item of FRUTAS){
+    console.log(item)
+
+}
+
+// é a mesma coisa que os outros exemplos de repeticao, porem de um modo mais simplificado
