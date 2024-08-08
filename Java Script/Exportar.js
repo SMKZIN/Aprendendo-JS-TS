447c7f7d884e99ca078f1a67d1ff5b1712495e41
smk <samueldavi6306@gmail.com>
2024-08-08 16:29:09 -0300

    m

---
+++ Java Script/Exportar.js
@@ -0,0 +1,8 @@
+function soma(num1, num2){
+
+    return num1 + num2
+
+}
+
+
+module.exports = soma // commonJS
---
+++ Java Script/Importar.js
@@ -0,0 +1,5 @@
+// commonJS ou es modules
+
+const soma = require("./Exportar") // commonJS
+
+import bibliotecaQualquer from "sjsjsjjs" // es modules
